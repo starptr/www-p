6d350d5b884bce345f9cd1f430520940ef4c9868
Yuto Nishida <starptr@users.noreply.github.com>
2021-08-08 00:00:39 -0700

    Remove utils import

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Main from "./pages/Main.svelte";
 	import Projects from "./pages/Projects.svelte";
-    import { enumToStrMapFactory } from "./lib/utils";
 
     const Pages = [
         "Main", "Projects", "Contact",
